50abb99fc72f54111b7e9e09b96e5113b6d0ffa4
asalex04 <arc04@mail.ru>
2018-03-14 10:41:11 +0400

    created code

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,15 +1,16 @@
+from getpass import getpass
 import vk
 
 
-APP_ID = -1  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
+APP_ID = 6406849
 
 
 def get_user_login():
-    pass
+    return input('input login: ')
 
 
 def get_user_password():
-    pass
+    return getpass('input password: ')
 
 
 def get_online_friends(login, password):
@@ -17,16 +18,30 @@ def get_online_friends(login, password):
         app_id=APP_ID,
         user_login=login,
         user_password=password,
+        scope='friends'
     )
-    api = vk.API(session)
-    # например, api.friends.get()
+    api = vk.API(session, v=5.52)
+
+    return api.users.get(
+        user_ids=api.friends.getOnline(),
+        fields='first_name, last_name'
+    )
 
 
 def output_friends_to_console(friends_online):
-    pass
+    print('\n Friends online:\n')
+    for friend in friends_online:
+        print('{} {}'.format(
+            friend['first_name'],
+            friend['last_name']
+        ))
+
 
 if __name__ == '__main__':
-    login = get_user_login()
-    password = get_user_password()
-    friends_online = get_online_friends(login, password)
-    output_friends_to_console(friends_online)
+    try:
+        login = get_user_login()
+        password = get_user_password()
+        friends_online = get_online_friends(login, password)
+        output_friends_to_console(friends_online)
+    except vk.exceptions.VkAuthError:
+        print('Incorrect login or password. Try again.')
